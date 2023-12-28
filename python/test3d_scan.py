521b40a11094d43de4120f07cbebdf8ade0d370d
z77 <reamicoy@gmail.com>
2023-12-28 14:46:16 +0100

    loop single frame loop batch frame

--- python/test3d_scan.py
+++ python/test3d_scan.py
@@ -5,8 +5,6 @@ import pickle
 import sys
 sys.path.append('C:/Users/zou/PycharmProjects/building-mapping/')
 
-from facade import Facade
-
 import numpy as np
 import open3d as o3d
 import time
@@ -68,38 +66,20 @@ def read_ply_file(
         raise Exception("Can't find ply file!")
     return data, np.asarray([float(nx), float(ny), float(nz)])
 
+def train_gp(gp, t_record):
+    # training GP:
+    tic = time.perf_counter()
+    gp.trainGPs()
+    toc = time.perf_counter()
+    print(f"Train time: {toc - tic:0.4f} seconds...")
+    t_record['frame'].append('-10')
+    t_record['train'].append(toc - tic)
 
-def main():
 
-    filepath = '//koko/qianqian/recording_Town10HD/dense_no_occlusion/'
-    with open('//koko/qianqian/recording_Town10HD/recording_Town10HD_Opt_2023-09-05_14_38.json', 'r') as file:
-        traj = json.load(file)
-    frameid = traj.keys()
-
-    data, _ = read_ply_file(filepath + 'scanstrips/all_frames.ply',
-                   dtype=np.dtype([("x", "<f4"), ("y", "<f4"), ("z", "<f4"), ("labels", "I"), ("frameID", "I")]))
-    obj = o3d.io.read_point_cloud(filepath + 'train_pts/building_pc_noisy_sample2.ply')
-    aer = np.c_[data['x'],data['y'], data['z']]
-    
-    t_record = {}
-    t_record['frame'] = []
-    t_record['train'] = []; t_record['train_number'] = []
-    t_record['test'] = []; t_record['test_number'] = []
-    tname = 'GPIS'; bias = 0.2
-
-    # set mapping box
-    center = obj.get_center()
-
-    test_xyz = np.array(o3d.io.read_point_cloud(filepath + 'test_pts/resolution_0.05/test_pts_sample2_sdf.ply').points)
-    gp_cloud_dir = filepath + '/output/gpismap/meta_data/frames/'
-    os.makedirs(gp_cloud_dir, exist_ok=True)
-
-    gp = GPisMap3D()
-    if gp.loggp:
-        tname = 'LogGPIS'
-        bias = 0
-
+def loop_single_frame(gp, data, traj, center, t_record):
     cc = 0
+    aer = np.c_[data['x'],data['y'], data['z']]
+    frameid = traj.keys()
     for k in frameid:
         if (int(k) <930) or (int(k)>1000):
             continue
@@ -140,22 +120,97 @@ def main():
         cc += 1
         if cc == 50:
             cc = 0
-            # training GP:
-            tic = time.perf_counter()
-            gp.trainGPs()
-            toc = time.perf_counter()
-            print(f"Train time: {toc - tic:0.4f} seconds...")
-            t_record['frame'].append('-10')
-            t_record['train'].append(toc - tic)
+            train_gp(gp, t_record)  # training GP
+    return gp, k, cc
+
+def loop_batch_frame(gp, data, traj, center, t_record, bsize=20):
+    cc = 0; bb = 0
+    xyz = np.c_[data['x'], data['y'], data['z']]
+    frameid = list(traj.keys())
+    for k in frameid[::bsize]:
+        rx = 0.00175 * 3
+        ry = 0.0175 * 1.2
+        print(f"#frame: {k}")
+        t_record['frame'].append(k)
+
+        ik = int(k)
+        tr = np.array([0., 0., 0.])
+        valid = np.zeros(len(data['frameID']), dtype=bool)
+        for i in range(bsize):
+            iki = ik + i
+            s = f"{iki}"
+            tr += np.array(traj[s]['ego_transformation'])[:3]
+            valid = valid | (data['frameID'] == iki)
+
+        tr /= bsize
+        dataz = xyz[valid]
+        if len(dataz) <= 0:
+            continue
+        vec = center - tr[:3]
+        a0 = np.arctan2(vec[1], vec[0])
+
+        tr = tr.flatten().astype(np.float32)
+        Rot = (np.array([[0, 1, 0], [0, 0, 1], [1, 0, 0]]) @
+               np.array([[np.cos(-a0), -np.sin(-a0), 0], [np.sin(-a0), np.cos(-a0), 0], [0, 0, 1]])).flatten().astype(
+            np.float32)
+
+        uvk = Rot.reshape(3, 3).T
+        pts_local = (xyz - tr) @ uvk
+        pts = pts_local[pts_local[:, 2] > 0]
+
+        u = pts[:, 0] / pts[:, 2]
+        v = pts[:, 1] / pts[:, 2]
+        valid2 = np.abs(u) < np.tan(0.25 * 3.14)
+
+        img, cxy, minx, miny = trans_to_img(np.c_[u, v, pts[:, 2]][valid2], resolx=rx, resoly=ry)
+
+        gp.set_cam_param(rx, ry, cxy[0], cxy[1], img.shape[1], img.shape[0])
+        tic = time.perf_counter()
+        gp.update_scan(img.astype(np.float32), tr, Rot)
+        # gp.add_scan(img.astype(np.float32), tr, Rot)
+        toc = time.perf_counter()
+        print(f"Elapsed time: {toc - tic:0.4f} seconds...")
+        t_record['train'].append(toc - tic)
+        t_record['train_number'].append(len(dataz[valid2]))
+        cc += 1
+        if cc == 50:
+            cc = 0
+            train_gp(gp, t_record)  # training GP
+    return gp, k, cc
+
+def main():
+
+    filepath = '//koko/qianqian/recording_Town10HD/dense_no_occlusion/'
+    with open('//koko/qianqian/recording_Town10HD/recording_Town10HD_Opt_2023-09-05_14_38.json', 'r') as file:
+        traj = json.load(file)
+
+    data, _ = read_ply_file(filepath + 'scanstrips/all_frames.ply',
+                   dtype=np.dtype([("x", "<f4"), ("y", "<f4"), ("z", "<f4"), ("labels", "I"), ("frameID", "I")]))
+    obj = o3d.io.read_point_cloud(filepath + 'train_pts/building_pc_noisy_sample2.ply')
+    
+    t_record = {}
+    t_record['frame'] = []
+    t_record['train'] = []; t_record['train_number'] = []
+    t_record['test'] = []; t_record['test_number'] = []
+    tname = 'GPIS'; bias = 0.2
+
+    # set mapping box
+    center = obj.get_center()
+
+    test_xyz = np.array(o3d.io.read_point_cloud(filepath + 'test_pts/resolution_0.05/test_pts_sample2_sdf.ply').points)
+    gp_cloud_dir = filepath + '/output/gpismap/meta_data/frames/'
+    os.makedirs(gp_cloud_dir, exist_ok=True)
+
+    gp = GPisMap3D()
+    if gp.loggp:
+        tname = 'LogGPIS'
+        bias = 0
+
+    # loop for each frame
+    gp, k, cc = loop_single_frame(gp, data, traj, center, t_record)
 
     if cc != 0:
-        # training GP:
-        tic = time.perf_counter()
-        gp.trainGPs()
-        toc = time.perf_counter()
-        print(f"Train time: {toc - tic:0.4f} seconds...")
-        t_record['frame'].append('-10')
-        t_record['train'].append(toc - tic)
+        train_gp(gp, t_record)
 
     # testing:
     tic = time.perf_counter()
